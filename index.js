8117bfcad86ad26c80d5e48362bf2cbe0c1082d2
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-09-26 22:37:07 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Brian Lawi';
+let welcomeMessage = "Welcome to Flatbook, Brian Lawi!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, BRIAN LAWI!"
+let shortGreeting = "Welcome, B!"
